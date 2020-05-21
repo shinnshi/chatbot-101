c5316d893f68b5ba8307e2179afa15e4df46b2df
shinnshi <shin.nshi3@gmail.com>
2020-05-21 21:25:32 +0900

    create rest api and cloud function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 import './assets/styles/index.css'
-import defaultDataset from "./dataset"
 import './assets/styles/style.css'
 import { AnswersList, Chats, FormDialog } from './compoents/index'
-
+import { db } from './firebase/index'
 export default class App extends Component {
   constructor(props) {
     super(props)
@@ -11,7 +10,7 @@ export default class App extends Component {
       answers: [],
       chats: [],
       currentId: "init",
-      dataset: defaultDataset,
+      dataset: {},
       open: false
     }
     this.handleClose = this.handleClose.bind(this)
@@ -67,10 +66,27 @@ export default class App extends Component {
         break
     }
   }
+  initDataset = (dataset) => {
+    this.setState({
+      dataset: dataset
+    })
+  }
 
   componentDidMount() {
-    const initAnswer = ""
-    this.selectAnswer(initAnswer, this.state.currentId)
+    (async () => {
+      const dataset = this.state.dataset
+      await db.collection('questions').get().then(snapshots => {
+        snapshots.forEach(doc => {
+          const id = doc.id
+          const data = doc.data()
+          dataset[id] = data
+        })
+      })
+      this.initDataset(dataset)
+      const initAnswer = ""
+      this.selectAnswer(initAnswer, this.state.currentId)
+    })()
+
   }
   componentDidUpdate() {
     const scrollArea = document.getElementById('scroll-area')
---
+++ src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDA3UlRmma2sgnK9DkjF2IPncPzft3_ujo",
+  authDomain: "chatbot-101-7fb00.firebaseapp.com",
+  databaseURL: "https://chatbot-101-7fb00.firebaseio.com",
+  projectId: "chatbot-101-7fb00",
+  storageBucket: "chatbot-101-7fb00.appspot.com",
+  messagingSenderId: "532239997754",
+  appId: "1:532239997754:web:9e2988699e09c662584e03",
+  measurementId: "G-2BFPYK7T1W"
+};
+
+export default firebaseConfig;
